4bce4d905be61fae8039194f63d8c9ce5d4a5fb8
Serhii Matviienko <smatvienko@outlook.com>
2019-09-02 15:27:39 +0300

    Form data assigment

--- src/TvViewContact.js
+++ src/TvViewContact.js
@@ -1,56 +1,135 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-import './TvViewContact.css';
+import "./TvViewContact.css";
 
 const TvInput = ({
-    id = '',
-    type='text',
-    placeholder
+  id = "",
+  name,
+  value,
+  type = "text",
+  placeholder,
+  onChangeHandler
 }) => {
-    return (
-        <div>
-            <label htmlFor={id}>{placeholder}</label>
-            <input id={id} type={type}/>
-        </div>
-    );
+  return (
+    <div>
+      <label htmlFor={id}>{placeholder}</label>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
 };
 
-const TvTextArea = ({
-    id = '',
-    placeholder
-}) => {
-    return (
-        <div>
-            <label htmlFor={id}>{placeholder}</label>
-            <textarea id={id}/>
-        </div>
-    );
+const TvTextArea = ({ id = "", name, value, placeholder, onChangeHandler }) => {
+  return (
+    <div>
+      <label htmlFor={id}>{placeholder}</label>
+      <textarea name={name} id={id} value={value} onChange={onChangeHandler} />
+    </div>
+  );
 };
 
 const TvViewContact = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    address: "",
+    description: "",
+    state: "",
+    link: "",
+    photos: []
+  });
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting !");
-    }
+  const _сhangeHandler = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-    return (
-        <div className="tv-contact-content">
-            <p>{t('contact.header')}</p>
-            <form className="tv-contactform" onSubmit={submitHandler}>
-                <TvInput id='1' type='email' placeholder={ t('contact.labels.email') }/>
-                <TvInput id='2' type='text' placeholder={ t('contact.labels.name') }/>
-                <TvInput id='3' type='text' placeholder={ t('contact.labels.address') }/>
-                <TvTextArea id='4' placeholder={t('contact.labels.description')}/>
-                <TvTextArea id='5' placeholder={t('contact.labels.state')}/>
-                <TvInput id='6' type='url' placeholder={ t('contact.labels.link') }/>
-                <button className="tv-send">{t('contact.send')}</button>
-                <TvInput id='7' type='text' placeholder={ t('contact.labels.photos') }/>
-            </form>
-        </div>
+    setFormData(previous =>
+      Object.assign(
+        {},
+        { ...previous },
+        {
+          [name]: value
+        }
+      )
     );
+  };
+
+  const _submitHandler = event => {
+    event.preventDefault();
+    console.log(formData);
+    alert("You are submitting !", formData);
+  };
+
+  return (
+    <div className="tv-contact-content">
+      <p>{t("contact.header")}</p>
+      <form className="tv-contactform" onSubmit={_submitHandler}>
+        <TvInput
+          id="1"
+          name="email"
+          value={formData.email}
+          type="email"
+          placeholder={t("contact.labels.email")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <TvInput
+          id="2"
+          name="name"
+          value={formData.name}
+          type="text"
+          placeholder={t("contact.labels.name")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <TvInput
+          id="3"
+          name="address"
+          value={formData.address}
+          type="text"
+          placeholder={t("contact.labels.address")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <TvTextArea
+          id="4"
+          value={formData.description}
+          name="description"
+          placeholder={t("contact.labels.description")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <TvTextArea
+          id="5"
+          value={formData.state}
+          name="state"
+          placeholder={t("contact.labels.state")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <TvInput
+          id="6"
+          name="link"
+          value={formData.link}
+          type="url"
+          placeholder={t("contact.labels.link")}
+          onChangeHandler={_сhangeHandler}
+        />
+        <button className="tv-send">{t("contact.send")}</button>
+        <TvInput
+          id="7"
+          name="photos"
+          value={formData.photos}
+          type="text"
+          placeholder={t("contact.labels.photos")}
+          onChangeHandler={_сhangeHandler}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default TvViewContact;
