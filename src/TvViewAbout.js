7cb25fe8664e821625e5b15368df312d3f0bfe78
Serhii Matviienko <smatvienko@outlook.com>
2019-09-02 16:18:17 +0300

    Small fixes for about page component

--- src/TvViewAbout.js
+++ src/TvViewAbout.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import { useTranslation, Trans } from 'react-i18next';
+import React from "react";
+import { useTranslation, Trans } from "react-i18next";
 
-import './TvViewAbout.css';
+import "./TvViewAbout.css";
+
+import { serviceMail, serviceFbPage } from "./settings.json";
 
 const TvViewAbout = () => {
-    const { t } = useTranslation();
-    const mail = 'project.terrainvague@gmail.com';
-    const facebookPage = 'https://www.facebook.com/terrainvagueplatform/';
+  const { t } = useTranslation();
 
-    const text = t('about.text', { tvmap : 'terrainvaguemap', returnObjects: true }).map((par, index) => {
-        return (
-            <p key={`paragraph${index}`}>{par}</p>
-        );
-    });
+  // TODO: investigate if it is possible to render array of template string with placeholders of components
+  const text = t("about.text", {
+    tvmap: "terrainvaguemap",
+    returnObjects: true
+  }).map((par, index) => {
+    return <p key={`paragraph${index}`}>{par}</p>;
+  });
 
-    return (
-        <div className="tv-about-content">
-            <div className="tv-about-headline">
-                <Trans 
-                i18nKey='about.headline'
-                components={[
-                    <span className="tv"></span>,
-                    <span className="tv-map"></span>,
-                    <span className="tv-phonetics"></span>
-                ]}
-                />
-            </div>
-            <div className="tv-about-text">
-                {text}
-            </div>                
-            <div className="tv-about-feedback">
-                <Trans 
-                i18nKey='about.feedback'
-                values = {{ tvmail: mail }}
-                components = {[<a className="tv-hyperlink" href={facebookPage}>{facebookPage}</a>, <a className="tv-hyperlink" href={`mailto:${mail}`}>{mail}</a>]}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="tv-about-content">
+      <div className="tv-about-headline">
+        <Trans
+          i18nKey="about.headline"
+          components={[
+            <span className="tv"></span>,
+            <span className="tv-map"></span>,
+            <span className="tv-phonetics"></span>
+          ]}
+        />
+      </div>
+      <div className="tv-about-text">{text}</div>
+      <div className="tv-about-feedback">
+        <Trans
+          i18nKey="about.feedback"
+          values={{ tvmail: serviceMail }}
+          components={[
+            <a className="tv-hyperlink" href={serviceFbPage}>
+              {serviceFbPage}
+            </a>,
+            <a className="tv-hyperlink" href={`mailto:${serviceMail}`}>
+              {serviceMail}
+            </a>
+          ]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TvViewAbout;
